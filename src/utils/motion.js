493ea4c7269a6ce1f883761eb2da8d908daf0861
calebbenjin <calebben94@gmail.com>
2023-05-16 10:50:15 +0100

    added staggerContainer  animation  function

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -74,3 +74,16 @@ export const slideIn = (direction, type, delay, duration) => {
     },
   };
 };
+
+export const staggerContainer = (staggerChildren, delayChildren) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
+
